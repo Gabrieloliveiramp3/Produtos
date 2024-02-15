d12598ccb361b8df751fd495c41e01c4c8f99fc1
GabrielOliveira <fgabrielo@bnb.gov.br>
2024-02-14 22:12:33 -0300

    Add project files.

---
+++ Produto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Produto
+{
+    public string Nome;
+    public double Preco;
+    public int Estoque;
+
+    public Produto()
+    {
+    }
+
+    public Produto(string nome, double preco)
+    {
+        Nome = nome;
+        Preco = preco;
+        Estoque = 0;
+    }
+
+    public int Vender(int qtde)
+    {
+        if (Estoque - qtde >= 0)
+
+        Estoque -= qtde;
+        Console.WriteLine($"Venda de {qtde} {Nome}s realizada com sucesso");
+        return Estoque;
+        
+    }
+
+    public int Comprar(int qtde)
+    {
+        Estoque += qtde;
+        Console.WriteLine($"Compra de {qtde} {Nome}s realizada com sucesso");
+        return Estoque;
+    }
+    public string ObterResumo()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Nome : {Nome}");
+        sb.AppendLine($"Preco : {Preco}");
+        sb.AppendLine($"Estoque : {Estoque}");
+        return sb.ToString();
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class MainClass 
+{
+    public static void Main(string[] args) 
+    {
+        Produto p1 = new();
+        p1.Nome = "Abacaxi";
+        p1.Preco = 3.8;
+        p1.Comprar(40);
+        Console.WriteLine(p1.ObterResumo());
+        
+
+
+        Produto p2 = new("Melancia", 4.5);
+        p2.Comprar(70);
+        Console.WriteLine(p2.ObterResumo());
+
+        Produto p3 = new("Goiaba", 7.8);
+        p3.Comprar(90);
+        p3.Vender(30);
+        Console.WriteLine(p3.ObterResumo());
+
+    }
+}
